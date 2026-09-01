13519984524c60dd6ec2f530be62ec9d7f6a57d6
Aayush Dadlani <aayushdadlani856@gmail.com>
2026-09-01 16:54:09 +0530

    __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,2 +1,9 @@
-from service import routes
-from service.common import log_handlers
+"""
+Package Initialization
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from service import routes  # noqa: E402,F401
