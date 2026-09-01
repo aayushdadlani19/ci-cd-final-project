6528d2bd8381b8f801c3d7c847b31f4a803d6313
Aayush Dadlani <aayushdadlani856@gmail.com>
2026-09-01 16:57:38 +0530

    __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -6,4 +6,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from service.common import log_handlers
+log_handlers.init_logging(app, "gunicorn.error")
+
+from service.common import error_handlers  # noqa: E402,F401
 from service import routes  # noqa: E402,F401
