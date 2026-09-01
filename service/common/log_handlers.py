0547b6d0042a8b7e97c1da3b871bd9fb2f803231
Aayush Dadlani <aayushdadlani856@gmail.com>
2026-09-01 16:37:31 +0530

    log_handlers.py

--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -20,6 +20,7 @@ Log Handlers
 This module contains utility functions to set up logging
 consistently
 """
+
 import logging
 
 
@@ -29,10 +30,19 @@ def init_logging(app, logger_name: str):
     gunicorn_logger = logging.getLogger(logger_name)
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+
     # Make all log formats consistent
     formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+        (
+            "[%(asctime)s] "
+            "[%(levelname)s] "
+            "[%(module)s] "
+            "%(message)s"
+        ),
+        "%Y-%m-%d %H:%M:%S %z",
     )
+
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
+
     app.logger.info("Logging handler established")
